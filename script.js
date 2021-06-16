b0371ccd2a3e468c2d975ea794d8757bee3c1eaf
Amarjit Singh <62531546+amarjitmangarhia@users.noreply.github.com>
2021-06-16 16:54:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,36 @@
 "use strict";
 
-// let itemName = document.querySelector(".item-name");
-// let list = document.querySelector("#list");
-// const addButton = document.querySelector("button");
-// const h3 = document.querySelector("h3");
-
-// addButton.addEventListener("click", function () {
-//   let txt = itemName.value;
-
-//   if (txt === "") h3.textContent = "OPPS! Something Is Missing!";
-//   else {
-//     let li = document.createElement("li");
-
-//     li.innerHTML = txt;
-//     list.insertBefore(li, list.childNodes[1]);
-
-//     itemName.value = "";
-//   }
-// });
-
-// list.addEventListener("click", (e) => {
-//   if (e.target.tagName === "LI") {
-//     e.target.classList.toggle("checked");
-//   }
-// });
-
+const add = document.querySelector(".add");
+const button = document.querySelector(".button");
 const buttonToAdd = document.querySelector(".button-to-add");
 const floatingBox = document.querySelector(".floating-box");
 const addButton = document.querySelector(".add");
 const input = document.querySelector("input");
-const listOfItems = document.querySelector(".list-of-items");
+const myId = document.querySelector("#myId");
+const hide = document.querySelector(".hide");
 
-addButton.addEventListener("click", function () {
+myId.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  }
+});
+
+add.addEventListener("click", function () {
   if (input.value == "") {
     return;
   } else {
-    const html = `<li class="item">
-  <p class="item-name">${input.value}</p>
-  <div class="button">✔</div>
-</li>`;
-    listOfItems.insertAdjacentHTML("beforeend", html);
-    floatingBox.style.opacity = "0";
+    const html = `<div class="item">
+    <li>${input.value}</li>
+    <div class="button">✔</div>
+  </div>`;
+    myId.insertAdjacentHTML("beforeend", html);
+    input.value = "";
+    floatingBox.classList.toggle("hide");
   }
 });
 
 //show popup
-let i = true;
 buttonToAdd.addEventListener("click", function () {
-  if (i) {
-    floatingBox.style.opacity = "1";
-
-    i = false;
-  } else {
-    floatingBox.style.opacity = "0";
-    i = true;
-  }
+  floatingBox.classList.toggle("hide");
 });
