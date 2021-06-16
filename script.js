4cd87e20bd1b49b11f6c3478ecb2ab727a29f047
Amarjit Singh <62531546+amarjitmangarhia@users.noreply.github.com>
2021-06-16 17:14:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 "use strict";
 
+// let itemName = document.querySelector(".item-name");
+// let list = document.querySelector("#list");
+// const addButton = document.querySelector("button");
+// const h3 = document.querySelector("h3");
+
+// addButton.addEventListener("click", function () {
+//   let txt = itemName.value;
+
+//   if (txt === "") h3.textContent = "OPPS! Something Is Missing!";
+//   else {
+//     let li = document.createElement("li");
+
+//     li.innerHTML = txt;
+//     list.insertBefore(li, list.childNodes[1]);
+
+//     itemName.value = "";
+//   }
+// });
+
+// list.addEventListener("click", (e) => {
+//   if (e.target.tagName === "LI") {
+//     e.target.classList.toggle("checked");
+//   }
+// });
+
 const add = document.querySelector(".add");
 const button = document.querySelector(".button");
 const buttonToAdd = document.querySelector(".button-to-add");
@@ -8,9 +33,9 @@ const addButton = document.querySelector(".add");
 const input = document.querySelector("input");
 const myId = document.querySelector("#myId");
 const hide = document.querySelector(".hide");
+const item = document.querySelector(".item");
 
 myId.addEventListener("click", function (e) {
-  console.log(e.target);
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
   }
@@ -24,6 +49,7 @@ add.addEventListener("click", function () {
     <li>${input.value}</li>
     <div class="button">✔</div>
   </div>`;
+    item.classList.add("none");
     myId.insertAdjacentHTML("beforeend", html);
     input.value = "";
     floatingBox.classList.toggle("hide");
