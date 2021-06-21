e28de7d7f6ba2d6c5be19a1ceedcff0de8b88ea3
Amarjit Singh <amarjitmangarhia@gmail.com>
2021-06-21 14:50:46 +0530

    c

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ let count = 0;
 let responsive = window.matchMedia("(max-width: 500px)");
 const liToCheckTheLengthOfLiItems = document.getElementsByTagName("li");
 const countLi = document.querySelector(".count");
+const floatingEdit = document.querySelector(".floating-edit");
+const inputOfEdit = document.querySelector(".input-of-edit");
+const editButton = document.querySelector(".edit-button");
+let isEditOpen = false;
 
 myId.addEventListener("click", function (e) {
   // ADD CHECKED CLASS
@@ -26,28 +30,52 @@ myId.addEventListener("click", function (e) {
   // REMOVE THE ELEMENT
 
   if (e.target.tagName === "BUTTON") {
-    e.target.parentNode.remove();
+    const x = e.target.closest("li");
+    x.remove();
   }
 
   if (e.target.tagName === "BUTTON2") {
-    alert("This feature will be added Soon!");
+    floatingEdit.style.display = "block";
+    isEditOpen = true;
+    inputOfEdit.focus();
+    editButton.addEventListener("click", function () {
+      const p = e.target.closest("p");
+      if (inputOfEdit.value === "") return;
+      edit(p);
+    });
   }
 });
+
+//RENDER ELEMENTS ON CLICKING EDIT BUTTON
+
+const edit = function (p) {
+  try {
+    p.innerHTML = `${inputOfEdit.value} <button>&times;</button>
+      <button2>edit</button2>`;
+    inputOfEdit.value = "";
+    floatingEdit.style.display = "none";
+    isEditOpen = false;
+  } catch (ex) {
+    console.log("Something went wrong: " + ex);
+  }
+};
 
 // RENDER LI ON SCREEN
 const renderHtml = function () {
   const html = `<li>
-  <p>${input.value}</p>
-  <button>&times;</button>
-  <button2>edit</button2>
+  <p>
+    ${input.value}
+    <button>&times;</button>
+    <button2>edit</button2>
+  </p>
 </li>`;
   // li.classList.add("hide");
-  li.style.display = "none";
+  // li.style.display = "none";
   myId.insertAdjacentHTML("beforeend", html);
   input.value = "";
   hidePopup();
-  countLi.textContent = liToCheckTheLengthOfLiItems.length - 1;
-  if (liToCheckTheLengthOfLiItems.length == 8) {
+  countLi.textContent = liToCheckTheLengthOfLiItems.length;
+  if (liToCheckTheLengthOfLiItems.length == 7) {
     res();
   }
 };
@@ -78,6 +106,8 @@ input.addEventListener("keyup", function (e) {
 const hidePopup = function () {
   if (!floatingBox.classList.contains("hide")) {
     floatingBox.classList.add("hide");
+  } else if (isEditOpen) {
+    return;
   } else {
     floatingBox.classList.remove("hide");
     input.focus();
@@ -96,3 +126,34 @@ const res = function () {
     countLi.classList.add("count-mobile");
   }
 };
+
+/*
+//this piece of code updating mutiple values
+
+  if (e.target.tagName === "BUTTON2") {
+    const p = e.target.closest("p");
+    floatingEdit.style.display = "block";
+
+    editButton.addEventListener("click", function () {
+      console.log(inputOfEdit.value);
+      p.innerHTML = `<p>${inputOfEdit.value} <button>&times;</button>
+    <button2>edit</button2></p>`;
+      floatingEdit.style.display = "none";
+    });
+  }
+
+*/
+
+//this piece of code showing error of null
+// if (e.target.tagName === "BUTTON2") {
+//   floatingEdit.style.display = "block";
+
+//   editButton.addEventListener("click", function () {
+//     const p = e.target.closest("p");
+//     let inputValue = inputOfEdit.value;
+//     p.innerHTML = `${inputValue} <button>&times;</button>
+//       <button2>edit</button2>`;
+//     inputValue = "";
+//     floatingEdit.style.display = "none";
+//   });
+// }
